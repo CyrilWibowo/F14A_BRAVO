343c1644b16bdba34d012b452bc49bc237859a22
Cyril Wibowo <cd.wibowo@gmail.com>
2026-03-13 11:10:45 +1100

    added placeholder db file

---
+++ backend/src/db.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import AsyncLock from 'async-lock';
+
+// CHANGE THIS FILE TO ACTUAL DATABASE
+// I TOOK THIS FROM 6080
+
+const lock = new AsyncLock();
+const DATABASE_FILE = './database.json';
+
+let locations = {};
+
+try {
+  const data = JSON.parse(fs.readFileSync(DATABASE_FILE));
+  locations = data.locations || {};
+} catch {
+  console.log('WARNING: No database found, creating a new one');
+  fs.writeFileSync(DATABASE_FILE, JSON.stringify({ locations: {} }, null, 2));
+}
+
+const persist = () =>
+  new Promise((resolve, reject) => {
+    lock.acquire('db', () => {
+      try {
+        fs.writeFileSync(DATABASE_FILE, JSON.stringify({ locations }, null, 2));
+        resolve();
+      } catch (err) {
+        reject(new Error('Failed to write to database'));
+      }
+    });
+  });
+
+export const getLocation = async (code) => {
+  return locations[code] || null;
+};
+
+export const getAllLocations = async () => {
+  return Object.values(locations);
+};
+
+export const setLocation = async (code, data) => {
+  locations[code] = data;
+  await persist();
+};
